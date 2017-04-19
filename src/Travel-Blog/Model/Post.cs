ddba96da15b0d600e3d27c350c77f483630e1afc
Chad Durkin and Sam Kirsch <chad+sam@>
2017-04-19 09:03:22 -0700

    add database context

---
+++ src/Travel-Blog/Model/Post.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Travel_Blog.Model
+{
+    public class Post
+    {
+        [Key]
+        public int PostId { get; set; }
+        public string Name { get; set; }
+        public string Content { get; set; }
+        public string Type { get; set; }
+        public int LocationId { get; set; }
+        public virtual Location Location { get; set; }
+        public virtual ICollection<Tag> Tags { get; set; }
+    }
+}
---
+++ src/Travel-Blog/Model/TravelBlogDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Travel_Blog.Model
+{
+    public class TravelBlogDbContext : DbContext
+    {
+        public TravelBlogDbContext()
+        {
+
+        }
+
+        public DbSet<Location> Locations { get; set; }
+
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=TravelBlogWithMigrations;integrated security=True");
+        }
+
+        public TravelBlogDbContext(DbContextOptions<TravelBlogDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
